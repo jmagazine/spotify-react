c30ea3df437e63ba259cd8a30ea4d293b36ead6b
Josh <joshmagazine13@gmail.com>
2023-07-17 16:03:30 -0400

    build

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,7 @@
 "use strict";
-require("dotenv");
+require("dotenv").config({
+  path: "/etc/secrets/.env",
+});
 const express = require("express");
 const router = express.Router();
 const request = require("request");
